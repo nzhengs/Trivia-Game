3d3c3d5a208044f9bbbbd296bc45f6c4601661d5
Anjana Hengaju <an.hengaju@gmail.com>
2019-02-26 22:54:34 -0600

    Add logic to start game using modal

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,6 +19,44 @@ $(document).ready(function() {
 
   console.log(questionaires[2].question);
   console.log(questionaires[2].options);
+  startGame();
+  var i = 0;
+  var correctAnswer = 0;
+  var incorrectAnswer = 0;
+  
+  var timer;
+  function countDownTimer() {
+    var time = 0;
+    timer = setInterval(function() {
+      time++;
+      $("#timer").text("00:" + time.toString().padStart(2, "0"));
+    }, 1000);
+  }
+
+  function startGame() {
+    $("#gameStarter").modal("show");
+
+    $(".exist").click(function() {
+      countDownTimer();
+      displayAQuestionInInterval();
+    });
+  }
+
+  function displayAQuestionInInterval() {
+    i=0;
+    displayAQuestionaire(questionaires[i]);
+    i++;
+    var interval = setInterval(function() {
+      
+      displayAQuestionaire(questionaires[i]);
+      i++;
+      if (i > questionaires.length - 1) {
+        clearInterval(interval);
+        clearInterval(timer);
+        showResult();
+      }
+    }, 2000);
+  }
 
   function displayAQuestionaire(questionaire) {
     $(".question").text(questionaire.question);
@@ -32,36 +70,33 @@ $(document).ready(function() {
     $("#option4").val(questionaire.options[3]);
   }
 
-  var i = 0;
-  var correctAnswer = 0;
-  var incorrectAnswer = 0;
-
- var interval = setInterval(function() {
-    displayAQuestionaire(questionaires[i]);
-    i++;
-    if(i>questionaires.length-1){
-      clearInterval(interval);
-    }
-  }, 2000);
+  // var interval = setInterval(function() {
+  //   displayAQuestionaire(questionaires[i]);
+  //   i++;
+  //   if (i > questionaires.length - 1) {
+  //     clearInterval(interval);
+  //     showResult();
+  //   }
+  // }, 2000);
 
   $("input[name=options]").click(function() {
     var clicked = this.value;
-   var answer = questionaires[i-1].answer;
-   console.log(clicked);
-   console.log(answer);
-   if(clicked == answer){
-     correctAnswer=correctAnswer+1;
-   }
-   else{
-     incorrectAnswer=incorrectAnswer+1;
-   }
-    console.log(correctAnswer);
-    console.log(incorrectAnswer);
+    var answer = questionaires[i - 1].answer;
+    if (clicked == answer) {
+      correctAnswer = correctAnswer + 1;
+    } else {
+      incorrectAnswer = incorrectAnswer + 1;
+    }
   });
 
+  function showResult() {
+    $("#gameResult").modal("show");
+    $("#questions").text("Total Questions: "+ questionaires.length);
+    $("#correct").text("Correct Answers: "+ correctAnswer);
+    $("#incorrect").text("Incorrect Answers: " + incorrectAnswer);
+    
 
-
-
-
-
+    console.log(correctAnswer);
+    console.log(incorrectAnswer);
+  }
 });
